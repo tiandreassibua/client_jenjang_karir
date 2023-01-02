abb6bb4883a01818bd668286e9a55f5d77fc29c3
Andreas Sibua <93329612+tiandreassibua@users.noreply.github.com>
2023-01-02 01:27:35 +0000

    adding 404 page

---
+++ src/pages/404.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const NotFoundPage = () => (
+    <div>
+        <h1 style="align-items: center, justify-content: center">404: Not Found</h1>
+    </div>
+)
+
+export default NotFoundPage
