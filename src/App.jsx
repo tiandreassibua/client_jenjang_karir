218211151d9bf31d1a1f7eccb4e73144b855b203
Andreas Sibua <93329612+tiandreassibua@users.noreply.github.com>
2023-01-02 01:04:04 +0000

    update routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Home from './pages/Home';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Pegawai from './pages/Pegawai/Pegawai';
 import TambahPegawai from './pages/Pegawai/TambahPegawai';
 import TambahJenjangKarir from './pages/Pegawai/TambahJenjangKarir';
@@ -10,22 +10,32 @@ import Formik from './Formik';
 
 
 function App() {
-  const router = createBrowserRouter([
-    { path: '/', element: <Home /> },
-    { path: '/pegawai', element: <Pegawai /> },
-    { path: '/pegawai/detail/:nip', element: <DetailPegawai /> },
-    { path: '/pegawai/tambah', element: <TambahPegawai /> },
-    { path: '/pegawai/jenjang/tambah/:nip', element: <TambahJenjangKarir /> },
-    { path: '/formik', element: <Formik /> },
-
-    { path: '/golongan', element: <Golongan /> },
-  ]);
+  // const router = createBrowserRouter([
+  //   { path: '/', element: <Home /> },
+  //   { path: '/pegawai', element: <Pegawai /> },
+  //   { path: '/pegawai/detail/:nip', element: <DetailPegawai /> },
+  //   { path: '/pegawai/tambah', element: <TambahPegawai /> },
+  //   { path: '/pegawai/jenjang/tambah/:nip', element: <TambahJenjangKarir /> },
+  //   { path: '/formik', element: <Formik /> },
+  //   { path: '/golongan', element: <Golongan /> },
+  // ]);
   
   
   return (
-    <div>
-      <RouterProvider router={router} />
-    </div>
+    // <div>
+    //   <RouterProvider router={router} />
+    // </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pegawai" element={<Pegawai />} />
+        <Route path="/pegawai/detail/:nip" element={<DetailPegawai />} />
+        <Route path="/pegawai/tambah" element={<TambahPegawai />} />
+        <Route path="/pegawai/jenjang/tambah/:nip" element={<TambahJenjangKarir />} />
+        <Route path="/formik" element={<Formik />} />
+        <Route path="/golongan" element={<Golongan />} />
+      </Routes>
+    </Router>
   );
 }
 
