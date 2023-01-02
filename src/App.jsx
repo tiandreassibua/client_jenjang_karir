00bd30eff86ecb4dacd5f28c91b7b62083379966
Andreas Sibua <93329612+tiandreassibua@users.noreply.github.com>
2023-01-02 01:17:45 +0000

    update main

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,15 @@
 import React from 'react'
 import Home from './pages/Home';
-import { createBrowserRouter, RouterProvider, BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Pegawai from './pages/Pegawai/Pegawai';
 import TambahPegawai from './pages/Pegawai/TambahPegawai';
 import TambahJenjangKarir from './pages/Pegawai/TambahJenjangKarir';
 import DetailPegawai from './pages/Pegawai/DetailPegawai';
 import Golongan from './pages/Golongan/Golongan';
-import Formik from './Formik';
 
 
 function App() {
-  // const router = createBrowserRouter([
-  //   { path: '/', element: <Home /> },
-  //   { path: '/pegawai', element: <Pegawai /> },
-  //   { path: '/pegawai/detail/:nip', element: <DetailPegawai /> },
-  //   { path: '/pegawai/tambah', element: <TambahPegawai /> },
-  //   { path: '/pegawai/jenjang/tambah/:nip', element: <TambahJenjangKarir /> },
-  //   { path: '/formik', element: <Formik /> },
-  //   { path: '/golongan', element: <Golongan /> },
-  // ]);
-  
-  
   return (
-    // <div>
-    //   <RouterProvider router={router} />
-    // </div>
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
@@ -32,7 +17,6 @@ function App() {
         <Route path="pegawai/detail/:nip" element={<DetailPegawai />} />
         <Route path="pegawai/tambah" element={<TambahPegawai />} />
         <Route path="pegawai/jenjang/tambah/:nip" element={<TambahJenjangKarir />} />
-        <Route path="formik" element={<Formik />} />
         <Route path="golongan" element={<Golongan />} />
       </Routes>
     </Router>
